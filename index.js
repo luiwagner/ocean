54ad2385bb95e94fdf3294b6253258f788a7da8c
Luis Wagner <github@luiswagner.com>
2024-08-26 20:10:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const letters = "abcdefghidklmnopqrstefwxyz";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
+
+
+$(".octopus_text").hover(function() {
+    let singleLetter = $(".octopus_text").split()
+    console.log(singleLetter)
+});
